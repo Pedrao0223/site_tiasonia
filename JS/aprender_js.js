0f4bd6d13d6d6595c27026580a718b2500698108
Pedro Henrique da Silva Vasconcelos <pedro60976326@edu.df.senac.br>
2026-05-12 17:06:38 -0300

    javas

---
+++ JS/aprender_js.js
@@ -0,0 +1,47 @@
+
+
+// Banco de dados 
+const nomeUsuario1 = "user@gmail.com"
+const senhaUsuario1 = "123456"
+
+const nomeUsuario2 = "user2@gmail.com"
+const senhaUsuario2 = "123456"
+
+
+// Variaveis de login
+let nomeDigitado = prompt("Diigite o seu email:")
+let nomeSenha = prompt("Diigite o sua senha:")
+
+// Verificacao de login 
+if (nomeDigitado === nomeUsuario1 && senhaUsuario1 === senhaUsuario1)
+{
+    alert("login bem-sucedido! bem-vindo," + nomeUsuario1 + "!")
+}else if(nomeDigitado === nomeUsuario2 && senhaUsuario1 === senhaUsuario2)
+{
+    alert("login bem-sucedido! bem-vindo," + nomeUsuario1 + "!")
+}else{
+    
+    alert("login falhou! Email ou senha incorretas")
+    document.body.classList.add("acesso_negado")
+    document.getElementById("banner__image").src = "img/"
+} 
+
+
+
+
+
+
+/*
+let idadeUsuario = prompt("Digite sua idade")
+let RendaUsuario = prompt("Digite a sua renda") 
+
+
+if(idadeUsuario >= 18 && RendaUsuario >= 1000){
+
+    alert("Usuario pode entrar")
+}else if(idadeUsuario <= 18 && RendaUsuario <= 1000){
+
+    alert("infelizmente nao pode acessar")
+}
+*/
+
